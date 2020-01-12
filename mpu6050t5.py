57128718baa3bc9c660dec1a888321bfdb35af0a
lawrence-su123 <49249611+lawrence-su123@users.noreply.github.com>
2020-01-11 19:53:37 -0800

    Add files via upload

--- mpu6050t5.py
+++ mpu6050t5.py
@@ -1,7 +1,32 @@
 import smbus			#import SMBus module of I2C
-from time import sleep          #import
+from time import sleep
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 
-#some MPU6050 Registers and their Address
+cred = credentials.Certificate('/home/pi/Downloads/crash-maps-firebase-adminsdk-mc1oh-3d46644415.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://crash-maps.firebaseio.com'
+})
+
+ref = db.reference('crash-maps')
+users_ref = ref.child('users')
+users_ref.set({
+         "Lawrence": {
+             "crashed":False,
+                },
+
+        })
+print(ref.get())
+#default.app = firebase_admin.initalize.app()
+#firebase=firebase.FirebaseApplication('https://crash-map.firebaseio.com')
+#result=firebase.post('https://crash-map.firebaseio.com', {'crashed' : True})
+#cred = credentials.ApplicationDefault()
+#firebase_admin.initialize_app(cred, {
+#    'projectId':'crash-maps',
+#    })
+#db = firestore.client()
+
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
@@ -16,32 +41,32 @@ GYRO_ZOUT_H  = 0x47
 
 
 def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-	
-	#Write to power management register
-	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-	
-	#Write to Configuration register
-	bus.write_byte_data(Device_Address, CONFIG, 0)
-	
-	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-	
-	#Write to interrupt enable register
-	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+    #write to sample rate register
+        bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+
+        #Write to power management register
+        bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+
+        #Write to Configuration register
+        bus.write_byte_data(Device_Address, CONFIG, 0)
+
+        #Write to Gyro configuration register
+        bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+
+        #Write to interrupt enable register
+        bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
 def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
+    #Accelero and Gyro value are 16-bit
         high = bus.read_byte_data(Device_Address, addr)
         low = bus.read_byte_data(Device_Address, addr+1)
-    
+
         #concatenate higher and lower value
         value = ((high << 8) | low)
-        
+
         #to get signed value from mpu6050
         if(value > 32768):
-                value = value - 65536
+            value = value - 65536
         return value
 
 
@@ -51,28 +76,42 @@ Device_Address = 0x68   # MPU6050 device address
 MPU_Init()
 
 print (" Reading Data of Gyroscope and Accelerometer")
-
+#doc_ref = db.collection(u'crashDetection').document(u'ligma')
+#doc_ref.set({
+#    u'crashed': true
+#    })
+avgx=[0,0,0,0,0]
+avgy=[0,0,0,0,0]
+avgz=[0,0,0,0,0]
 while True:
-	
-	#Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
-	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
-	
 
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-	sleep(1)
+    #Read Accelerometer raw value
+        acc_x = read_raw_data(ACCEL_XOUT_H)
+        acc_y = read_raw_data(ACCEL_YOUT_H)
+        acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+        #Read Gyroscope raw value
+        gyro_x = read_raw_data(GYRO_XOUT_H)
+        gyro_y = read_raw_data(GYRO_YOUT_H)
+        gyro_z = read_raw_data(GYRO_ZOUT_H)
+
+        #Full scale range +/- 250 degree/C as per sensitivity scale factor
+        Ax = acc_x/16384.0
+        Ay = acc_y/16384.0
+        Az = acc_z/16384.0
+
+        Gx = gyro_x/75
+        Gy = gyro_y/75
+        Gz = gyro_z/75
+        if Gx>15 or Gy>15 or Gz>15:
+            print ("u crashed lmao")
+            users_ref.set({
+                "Lawrence": {
+                    "crashed":True,
+                },
+
+        })
+            print(ref.get())
+        
+        print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+        sleep(1)
