3fe3dd6e7e62f5aef2a4fe35a2c95d7d42abac14
lawrence-su123 <49249611+lawrence-su123@users.noreply.github.com>
2020-01-12 07:25:29 -0800

    Update and rename mpu6050.py to gyrotocsv.py

--- gyrotocsv.py
+++ gyrotocsv.py
@@ -1,5 +1,5 @@
 
-import smbus			#import SMBus module of I2C
+import smbus			
 from time import sleep
 import csv
 import bluepy
@@ -20,39 +20,28 @@ GYRO_ZOUT_H  = 0x47
 def MPU_Init():
 
 	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-
-	#Write to power management register
 	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-
-	#Write to Configuration register
 	bus.write_byte_data(Device_Address, CONFIG, 0)
-
-	#Write to Gyro configuration register
 	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-
-	#Write to interrupt enable register
 	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
 def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
         high = bus.read_byte_data(Device_Address, addr)
         low = bus.read_byte_data(Device_Address, addr+1)
 
-        #concatenate higher and lower value
         value = ((high << 8) | low)
 
-        #to get signed value from mpu6050
+     
         if(value > 32768):
                 value = value - 65536
         return value
 
 
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
+bus = smbus.SMBus(1) 	
+Device_Address = 0x68   
 
 MPU_Init()
 
-print (" Reading Data of Gyroscope and Accelerometer")
 ax = array.array[0, 0, 0, 0, 0, 0]
 ay = array.array[0, 0, 0, 0, 0, 0]
 az = array.array[0, 0, 0, 0, 0, 0]
